96a9390e001a3085db98c867e85d515dfd8f7ea4
pasquawisdom2007-beep <2.6819725e+08+pasquawisdom2007-beep@users.noreply.github.com>
2026-09-07 01:33:16 +0000

    fix: repair anime reaction GIF parsing

--- commands/fun/kill.js
+++ commands/fun/kill.js
@@ -1,7 +1,7 @@
 const { makeAnimeReaction } = require('../../lib/animeReaction');
 module.exports = makeAnimeReaction({
     name: 'kill', emoji: '🔪', verb: 'killed', selfVerb: 'is plotting a murder',
-    reaction: 'punch',
+    reaction: 'kill',
     fallbacks: [
         'https://media.giphy.com/media/xT0BKiwiVJq5B0XhHG/giphy.gif',
         'https://media.giphy.com/media/Zau0yrl17uzdK/giphy.gif'
